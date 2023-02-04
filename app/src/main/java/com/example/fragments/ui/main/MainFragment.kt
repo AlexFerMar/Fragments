02b6c5766b22e5e1bfbe65d559e50337eccabc81
AlexFerMar <91198425+AlexFerMar@users.noreply.github.com>
2023-02-04 16:17:09 +0100

    Creada funcionaliad del primer fragmento

---
+++ app/src/main/java/com/example/fragments/ui/main/MainFragment.kt
@@ -0,0 +1,52 @@
+package com.example.fragments.ui.main
+
+import android.graphics.Bitmap
+import android.os.Bundle
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import com.example.fragments.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+
+    private lateinit var ivFoto: ImageView
+
+
+    private lateinit var viewModel: MainViewModel
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        var view = inflater.inflate(R.layout.fragment_main, container, false)
+        ivFoto = view.findViewById(R.id.ivFoto)
+
+        return view
+    }
+
+
+    fun changePhoto(photo: Bitmap?) {
+
+        ivFoto.setImageBitmap(photo)
+
+    }
+
+
+}
